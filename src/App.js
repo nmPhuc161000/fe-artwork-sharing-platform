36a7efdcd7cd3b49b5a90d2b1ee154758b84d1a2
BaoNN <baonnse161010@gmail.com>
2024-02-21 22:12:44 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Footer from './component/footer/Footer';
 import Header from './component/header/Header';
 import Login from './component/header/login-pages/Login';
 import Register from './component/header/register/Register';
+import Home from './component/body/home-pages/Home'
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
-      <Footer/>
-      {/* <Login/> */}
+      <Header/>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/regis" element={<Register/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
