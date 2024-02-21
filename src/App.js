c103be7256f8a7c2305bdc6c10b616a368990107
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2024-02-21 22:03:20 +0700

    update login page and regis page, add header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Header from './component/header/Header';
 import Login from './component/header/login-pages/Login';
 import Register from './component/header/register/Register';
+import Home from './component/body/home-pages/Home'
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
-      <Register/>
-      {/* <Login/> */}
+      <Header/>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/regis" element={<Register/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Icon } from "react-materialize";
+import { Icon, Button } from "react-materialize";
 import { Link } from "react-router-dom";
+import './Header.css'
 
 export default function Header() {
   return (
@@ -8,6 +9,14 @@ export default function Header() {
       <div className="navbar">
         <div className="search">
           <Icon>search</Icon>
+          <input placeholder="Search"/>
+        </div>
+        <div className="logo">
+          <Link to={`home`}><img src="./assets/image/logo.png" alt=""/></Link>
+        </div>
+        <div className="">
+          <Link to={`login`}><Button>Login</Button></Link>
+          <Link to={`regis`}><Button>Register</Button></Link>
         </div>
       </div>
     </>
