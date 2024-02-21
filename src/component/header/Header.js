b14d8316a7bab85c20277c4e57ccd100453ce6e8
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2024-02-21 17:50:21 +0700

    update register

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "react-materialize";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div className="navbar">
+        <div className="search">
+          <Icon>search</Icon>
+        </div>
+      </div>
+    </>
+  );
 }
