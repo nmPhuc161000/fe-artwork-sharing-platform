1d7a7580e7d56ea0bc9b68c1b445a94e0a2a8a63
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2024-02-22 09:06:19 +0700

    add picture and update file header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { Icon, Button } from "react-materialize";
 import { Link } from "react-router-dom";
-import './Header.css'
+import "./Header.css";
 
 export default function Header() {
   return (
     <>
-      <div className="navbar">
-        <div className="search">
-          <Icon>search</Icon>
-          <input placeholder="Search" type="text"/>
-        </div>
-        <div className="logo">
-          <Link to={`/`}><img src="./assets/image/logo.png" alt=""/></Link>
-        </div>
-        <div className="regisLogin">
-          <Link to={`login`}><button>Login</button></Link>
-          <Link to={`regis`}><button>Register</button></Link>
+      <div className="nav">
+        <div className="navbar">
+          <div className="search">
+            <Icon>search</Icon>
+            <input placeholder="Search" type="text" />
+          </div>
+          <div className="logo">
+            <Link to={`/`}>
+              <img src="./assets/image/logo.png" alt="" />
+            </Link>
+          </div>
+          <div className="regisLogin">
+            <Link to={`login`}>
+              <button>Login</button>
+            </Link>
+            <Link to={`regis`}>
+              <button>Register</button>
+            </Link>
+            <Link to={`regis`}>
+              <img src="./assets/image/no-avatar.webp" alt="" />
+            </Link>
+          </div>
         </div>
       </div>
     </>
