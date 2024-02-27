1166a17a9599a5a1ea3cc3ea425366d61bd78e09
BaoNN <baonnse161010@gmail.com>
2024-02-27 16:19:50 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -9,11 +9,9 @@ import Home from './component/body/home-pages/Home'
 import Detail from './component/Details/Detail';
 import RecoveryPassword from './component/header/recovery-password/RecoveryPassword';
 import Profile from './component/profile/Profile';
-<<<<<<<<< Temporary merge branch 1
 import ProfileNav from './component/profile/profile-Nav/ProfileNav';
-=========
 import Payment from './component/payment/Payment';
->>>>>>>>> Temporary merge branch 2
+
 
 function App() {
   const location = useLocation();
@@ -32,26 +30,16 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-<<<<<<<<< Temporary merge branch 1
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/recovery-password" element={<RecoveryPassword />}></Route>
-        <Route path="/regis" element={<Register />}></Route>
-        <Route path="/detail" element={<Detail />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/profile" element={<Profile />}>
-            {/* Sử dụng đường dẫn tương đối cho ProfileNav */}
-            <Route path="nav" element={<ProfileNav />}></Route>
-        </Route>
-=========
         <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/recovery-password" element={<RecoveryPassword/>}></Route>
         <Route path="/regis" element={<Register/>}></Route>
         <Route path="/detail" element={<Detail/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/profile" element={<Profile />}>
+            {/* Sử dụng đường dẫn tương đối cho ProfileNav */}
+            <Route path="nav" element={<ProfileNav />}></Route>
+        </Route>
         <Route path="/payment" element={<Payment/>}></Route>
->>>>>>>>> Temporary merge branch 2
       </Routes>
       <Footer isLoginPage={isLoginPage} isRegisterPage={isRegisterPage} />
     </div>
